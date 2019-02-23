917e3d8dbdf8af898805feb95be0e68900ac3d67
Chris Bistline <chris.bistline@gmail.com>
2019-02-23 13:37:57 -0600

    Changes to question format

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,23 +23,24 @@ $(document).ready(function () {
     // OBJECTS
     // ==================================
 
-    function Questions(q1, a1, a2, a3, a4, correct) {
+    function Questions(q1, a1, a2, a3, a4, correct, expl) {
         this.questionText = q1;
         this.answera = a1;
         this.answerb = a2;
         this.answerc = a3;
         this.answerd = a4;
         this.correctAnswer = correct;
+        this.explained = expl;
     };
 
 
     // create some questions
-    Q1 = new Questions("Quarterback for the New England Patriots, who won the 2018 Associated Press NFL Most Valuable Player Award?", "a. Matt Ryan", "b. Aaron Rodgers", "c. Tom Brady", "d. Cam Newton", "c");
-    Q2 = new Questions("question 2 text", "answer 1", "answer 2", "answer 3", "answer 4", 2);
-    Q3 = new Questions("question 3 text", "answer 1", "answer 2", "answer 3", "answer 4", 3);
-    Q4 = new Questions("question 4 text", "answer 1", "answer 2", "answer 3", "answer 4", 4);
-    Q5 = new Questions("question 5 text", "answer 1", "answer 2", "answer 3", "answer 4", 1);
-    Q6 = new Questions("question 6 text", "answer 1", "answer 2", "answer 3", "answer 4", 2);
+    Q1 = new Questions("Quarterback for the New England Patriots, who won the 2018 Associated Press NFL Most Valuable Player Award?", "a. Matt Ryan", "b. Aaron Rodgers", "c. Tom Brady", "d. Cam Newton", "c", "Tom Brady - The AP award is considered the de facto official NFL MVP award.");
+    Q2 = new Questions("Which NHL team won the 2018 Stanley Cup finals against the Vegas Golden Knights?", "a. Nashville Predators", "b. Toronto Maple Leafs", "c. Washington Capitals", "d. Pittsburgh Penguins", "c", "Washington Capitals - Washington beat Vegas four games to one.");
+    Q3 = new Questions("Who was the National Basketball Association's Most Valuable Player (MVP) for the 2017-2018 season?", "a. Kristaps Porzingis", "b. Victor Oladipo", "c. James Harden", "d. Karl-Anthony Towns", "c", "James Harden - James Harden was the season's top scorer.");
+    Q4 = new Questions("Held in June at Shinnecock Hills Golf Club in New York, who won the 2018 U.S. Open Golf Championship? ", "a. Tommy Fleetwood", "b. Patrick Reed", "c. Brooks Koepka", "d. Dustin Johnson",  "c","Brooks Koepka - With the win, Brooks became the seventh player to win consecutive U.S. Opens.");
+    Q5 = new Questions("What NFL team won Super Bowl 52 by a score of 41 to 33 on February 4, 2018? ", "a. Green Bay Packers", "b. Philadelphia Eagles", "c. New England Patriots", "d. Dallas Cowboys", "b", "Philadelphia Eagles - The Philadelphia Eagles beat the New England Patriots. It was their first Super Bowl win.");
+    Q6 = new Questions("Held on April 26-28, who was the first pick in the first round at the 2018 NFL draft?", "a. Saquon Barkley", "b. Denzel Ward", "c. Sam Darnold", "d. Baker Mayfield", "d", "Baker Mayfield - Five quarterbacks were selected in the first round.");
 
     allQuestions = [Q1, Q2, Q3, Q4, Q5, Q6];
 
@@ -101,14 +102,14 @@ $(document).ready(function () {
         // tell user if they were right or wrong
         if (letter === currentQuestion.correctAnswer) {
             updateScore();
-            $("#questionArea").append("<h3>CORRECT!</h3>")
+            $("#questionArea").html("<h3>CORRECT!</h3>").append(currentQuestion.explained);
         } else {
-            $("#questionArea").append("<h3>WRONG ANSWER!</h3>")
+            $("#questionArea").html("<h3>INCORRECT!</h3>").append(currentQuestion.explained);
         }
         // run showAnswer function
         showAnswer(letter);
     };
-
+ 
     function showAnswer(letter) {
         // turn off hover function
         $("#answerTable").removeClass("table-hover");
@@ -134,7 +135,6 @@ $(document).ready(function () {
 
     function initGame() {
         //.removeClass() with no parameters to remove all, .addClass("...") to add back to default
-
         $("#answerTable").addClass("table-hover");
         $("#answera").removeClass();
         $("#answerb").removeClass();
